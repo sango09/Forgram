9f32c512bd8934e05e5847fd7a9ea98a1f22533a
Santiago Gomez Florez <58926096+sango09@users.noreply.github.com>
2020-01-05 22:07:01 -0500

    primer commit de posts

---
+++ posts/apps.py
@@ -0,0 +1,9 @@
+#Posts aplication module
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    #posts aplication settings
+    name = 'posts'
+    varbose_name = 'Posts'
---
+++ posts/views.py
@@ -0,0 +1,43 @@
+# Posts views
+
+# Django
+from django.shortcuts import render
+
+# utilities
+from datetime import datetime
+
+posts = [
+    {
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yessica cortes',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+
+    },
+    {
+        'title': 'Via Lactea',
+        'user':{
+            'name': 'C.vander',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        }, 
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo' : 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user':{
+            'name': 'Thespianartist',
+            'picture' : 'https://picsum.photos/60/60/?image=883'
+        }, 
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo':'https://picsum.photos/500/700/?image=1076',
+    }
+]
+
+
+def lists_posts(request):
+    # lists existing posts.
+    return render(request,'feed.html',{'posts': posts},)
