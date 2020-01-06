d11abfe72afab624798708414b42d9eec1ac46e9
Santiago Gomez Florez <58926096+sango09@users.noreply.github.com>
2020-01-05 22:01:42 -0500

    Primer commit

---
+++ platzigram/urls.py
@@ -0,0 +1,18 @@
+# Platzigram URLs module.
+#Django
+from django.urls import path
+from platzigram import views as local_views
+from posts import views as posts_views
+from django.contrib import admin
+
+
+urlpatterns = [
+
+
+    path('admin/',admin.site.urls),
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path("hi/<str:name>/<int:age>/",local_views.say_hi),
+
+    path('posts/',posts_views.lists_posts),
+]
