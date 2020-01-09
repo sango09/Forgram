d386800f060b3b1f478071fc38018948f41e6a4c
Santiago Gomez Florez <58926096+sango09@users.noreply.github.com>
2020-01-08 23:47:01 -0500

    Update a user's profile view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 # Users views
 
 # Django
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Exceptions
 from django.db.utils import IntegrityError
@@ -13,13 +13,45 @@ from django.db.utils import IntegrityError
 from django.contrib.auth.models import User
 from users.models import Profile
 
+# Forms
+from users.forms import ProfileForm
+
+def update_profile(request):
+    #Update a user's profile view.
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form 
+        }
+    )
+
+
 def login_view(request):
     # Login view.
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username,
-                            password=password)
+        user = authenticate(request, username=username,password=password)
 
         if user:
             login(request, user)
@@ -30,7 +62,7 @@ def login_view(request):
     return render(request, 'users/login.html')
 
 
-def signup_view(request):
+def signup(request):
     # Signup view
     if request.method == 'POST':
         username = request.POST['username']
@@ -42,7 +74,7 @@ def signup_view(request):
         try:
             user = User.objects.create_user(username=username, password=password,)
 
-        except(IntegrityError):
+        except IntegrityError:
             return render(request, 'users/signup.html', {'error': 'username is already in user '})
 
         user.first_name = request.POST['first_name']
