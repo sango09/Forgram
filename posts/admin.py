32893f2f324cfaf62eb43c64dd31d5047a6d7673
Santiago Gomez Florez <58926096+sango09@users.noreply.github.com>
2020-01-10 19:26:59 -0500

    Update admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,17 +1,16 @@
-#Posts model
+# Posts model.
 
-#Django
+# Django
 from django.contrib import admin
+
+# Models
 from posts.models import Post
 
 
 @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Post admin.
 
-
-class postAdmin(admin.ModelAdmin):
-    #Post admin
-
-    list_display = ('id','user','title','created','photo')
-    list_filter =['created','modified',]
-
-    search_fields = ['title', 'user__username', 'user__email',]
+    list_display = ('id', 'user', 'title', 'photo')
+    search_fields = ('title', 'user__username', 'user__email')
+    list_filter = ('created', 'modified')
